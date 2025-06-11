34f628ba6fe0afbb048033cd4d6671b3f3e2c7c1
Vlad Vozhzhov <vladvozhzhov@outlook.com>
2025-06-11 19:02:13 +0300

    Made some fixes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,12 +2,19 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import recipeRoutes from './routes/recipes';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use('/assets', express.static(path.resolve(__dirname, '../../frontend/dist/assets')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../../frontend/dist/index.html'));
+});
 
 app.use('/api/recipes', recipeRoutes);
 
